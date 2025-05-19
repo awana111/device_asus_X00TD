b84ac112134f1f676907c29650f8b48983be09fe
Kneba <abenkenary3@gmail.com>
2025-05-19 05:46:10 +0000

    X00TD: Project Infinity-X Bringup
    Change-Id: Ic751163e75806b8864270d4c49fe425b5b58ab64

---
+++ infinity_X00TD.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Infinity-X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Infinity-X stuff.
+BYPASS_CHARGE_SUPPORTED := true
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := qǝuʞz
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+TARGET_BUILD_GOOGLE_TELEPHONY := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SHIPS_FULL_GAPPS := false
+TARGET_SHIPS_GOOGLE_DIALER := false
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_MOTO_CALCULATOR := true
+WITH_GAPPS := false
+
+# Inherit some common device props
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := infinity_X00TD
+PRODUCT_DEVICE := X00TD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceProduct=ASUS_X00TD \
+    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
+    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export BUILD_USER="queen"
+export TZ="Asia/Jakarta"
+
+# Clone kernel tree
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b stable kernel/asus/sdm660
+
+# Clone common tree
+rm -rf device/asus/sdm660-common
+git clone --depth=1 https://github.com/Kneba/device_asus_sdm660-common -b qpr2 device/asus/sdm660-common
+
+# Clone vendor tree
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/Kneba/proprietary_vendor_asus -b qpr2 vendor/asus
+
+# Clone keys
+rm -rf vendor/lineage-priv/keys
+rm -rf vendor/lineage/signing/keys
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
