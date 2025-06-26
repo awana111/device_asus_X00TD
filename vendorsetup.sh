06ec1ce307cd07a80a9276e1c0db15d196bcd863
texascake <tiktodz@soulvibe-team.cloud>
2025-06-26 20:55:05 +0000

    sdm660-common: Update our trees

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,11 +8,11 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 
 # Clone common tree
 rm -rf device/asus/sdm660-common
-git clone https://github.com/Kneba/device_asus_sdm660-common -b newpart device/asus/sdm660-common
+git clone https://github.com/Kneba/device_asus_sdm660-common -b perf device/asus/sdm660-common
 
 # Clone vendor tree
 rm -rf vendor/asus
-git clone --single-branch https://github.com/Kneba/proprietary_vendor_asus -b newpart vendor/asus
+git clone --single-branch https://github.com/Kneba/proprietary_vendor_asus -b perf vendor/asus
 
 # Clone keys
 rm -rf vendor/lineage-priv/keys
